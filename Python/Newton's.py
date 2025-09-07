9207c93a930e2577b0e9ad2d4b041dbb1abab5de
Priyanshu-Prakhar <prakhar2.178@gmail.com>
2025-09-07 11:21:02 +0530

    Removed junk lines

--- Python/Newton's.py
+++ Python/Newton's.py
@@ -1,6 +1,7 @@
 #In this file, I'll try implementing Newton's Rootfinding Algorithm.
 #If this succeeds, I'll try writing it in C++.
-import os
+
+
 def x2_minus2(m):
     return (m**2) -2
 
@@ -16,7 +17,7 @@ def newton(f,x):
 
 abscissa = float(input("Enter the abscissa to calculate the derivative of x^2 : "))
 
-print(f"Derivative of f(x)=x^2 at x = {abscissa} = ", f"{d(x2_minus2, abscissa):.3f} \n")
+print(f"Derivative of f(x)=x^2 -2 at x = {abscissa} = ", f"{d(x2_minus2, abscissa):.3f} \n")
 
 # x_new = x_old - (f(x)/f'(x))
 step = abs(abscissa - newton(x2_minus2, abscissa))
@@ -47,5 +48,3 @@ while next == "":
 
     if next != "":
         break
-
-#guess = input("Enter your best guess of where the root may be nearby : ")
